92cf4434a0c9fc273e3130a1a2ccec4cb35e6361
Line Rosland <linehro@stud.ntnu.no>
2025-03-19 15:52:56 +0100

    debugge

--- w3tp/w3t/_scoff.py
+++ w3tp/w3t/_scoff.py
@@ -1606,9 +1606,7 @@ def filter_by_reference(static_coeff_1, static_coeff_2, static_coeff_3=None, thr
 
                 coeff_check1 = filter(static_coeff_1, this_threshold_low, scoff=name, single=True)[1]
                 coeff_check2 = filter(static_coeff_2, this_threshold_high, scoff=name, single=True)[1]
-                print("Shape of coeff_up_check - single :", coeff_check1.shape)
-                print("Shape of coeff_up_check - single :", coeff_check2.shape)
-
+                
 
 
                 has_nan_1 = np.any(np.isnan(coeff_check1[idx1]))
@@ -1656,9 +1654,7 @@ def filter_by_reference(static_coeff_1, static_coeff_2, static_coeff_3=None, thr
                     filter(static_coeff_2, this_threshold_med, scoff=name, single=False)[1],
                     filter(static_coeff_3, this_threshold_high, scoff=name, single=False)[1],
                 ]
-                for check in coeff_up_checks:
-                    print("Shape of coeff_up_check (non single):", check.shape)
-
+              
                 nan_flags_up = [np.any(np.isnan(check[idx])) for check, idx in zip(coeff_up_checks, [idx1, idx2, idx3])]
                 clean_idxs_up = [i for i, nan in enumerate(nan_flags_up) if not nan]
 
@@ -1689,7 +1685,6 @@ def filter_by_reference(static_coeff_1, static_coeff_2, static_coeff_3=None, thr
                     filter(static_coeff_2, this_threshold_med, scoff=name, single=False)[2],
                     filter(static_coeff_3, this_threshold_high, scoff=name, single=False)[2],
                 ]
-                print("Shapes of coeff_down_checks:", [check.shape for check in coeff_down_checks])
 
                 nan_flags_down = [np.any(np.isnan(check[idx])) for check, idx in zip(coeff_down_checks, [idx1, idx2, idx3])]
 
